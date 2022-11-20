db21b4f6cb0c8a557fa242a1cffa59036b0009b0
Florian Blasius <florian.blasius@slint-ui.com>
2022-11-20 14:46:06 +0100

    ci

--- apps/co_chat/src/main.rs
+++ apps/co_chat/src/main.rs
@@ -16,13 +16,7 @@ fn main() {
     #[cfg(all(debug_assertions, target_arch = "wasm32"))]
     console_error_panic_hook::set_once();
 
-    let ui = AppWindow::new();
-
-    let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
-    });
+    let ui = App::new();
 
     ui.run();
 }
