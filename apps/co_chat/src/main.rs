8d75097828b41cb6aa615148ba2b11711c14cb42
Florian Blasius <florian.blasius@slint-ui.com>
2022-11-21 08:57:25 +0100

    op ci
    ci split

--- apps/co_chat/src/main.rs
+++ apps/co_chat/src/main.rs
@@ -7,7 +7,11 @@
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
+use std::rc::Rc;
+
 slint::include_modules!();
+
+use slint::*;
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn main() {
@@ -17,6 +21,59 @@ pub fn main() {
     console_error_panic_hook::set_once();
 
     let ui = App::new();
+
+    let public_icon = ui.global::<mi>().get_public();
+    let lock_icon = ui.global::<mi>().get_lock();
+    let account_box = ui.global::<mi>().get_account_box();
+
+    let channels = Rc::new(VecModel::default());
+
+    // group channels,
+    let group_channels = Rc::new(VecModel::default());
+    group_channels.push(ItemModel {
+        text: "Town Square".into(),
+        leading_icon: public_icon.clone(),
+        ..Default::default()
+    });
+    group_channels.push(ItemModel {
+        text: "Internal".into(),
+        leading_icon: lock_icon.clone(),
+        ..Default::default()
+    });
+    channels.push(ParentItemModel {
+        text: "Channels".into(),
+        items: group_channels.into(),
+    });
+
+    // direct channels
+    let direct_channels = Rc::new(VecModel::default());
+    direct_channels.push(ItemModel {
+        text: "Simon".into(),
+        leading_icon: account_box.clone(),
+        ..Default::default()
+    });
+    direct_channels.push(ItemModel {
+        text: "Olivier".into(),
+        leading_icon: account_box.clone(),
+        ..Default::default()
+    });
+    direct_channels.push(ItemModel {
+        text: "Tobias".into(),
+        leading_icon: account_box.clone(),
+        ..Default::default()
+    });
+    direct_channels.push(ItemModel {
+        text: "Auri".into(),
+        leading_icon: account_box.clone(),
+        ..Default::default()
+    });
+    channels.push(ParentItemModel {
+        text: "DIRECT MESSAGES".into(),
+        items: direct_channels.into(),
+    });
+
+    ui.global::<AppSideBarBridge>()
+        .set_channels(channels.into());
 
     ui.run();
 }
